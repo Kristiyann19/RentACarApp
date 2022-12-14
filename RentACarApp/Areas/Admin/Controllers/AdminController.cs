755b01ca26ab809e0b2a2d45d835c9e9b26898bf
Kristiyann19 <karamilevkristiyan@gmail.com>
2022-12-14 15:12:20 +0200

    admin area

---
+++ RentACarApp/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static RentACarApp.Areas.Admin.Constants.AdminConstants;
+
+namespace RentACarApp.Areas.Admin.Controllers
+{
+    [Area(AreaName)]
+    [Authorize(Roles = AdminRoleName)]
+    public class AdminController : Controller
+    {
+         
+    }
+}
--- RentACarApp/Program.cs
+++ RentACarApp/Program.cs
@@ -4,6 +4,7 @@ using RentACarApp.Contracts;
 using RentACarApp.Data;
 using RentACarApp.Data.Entities;
 using RentACarApp.Services;
+using RentACarApp.ChatHub;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +20,7 @@ builder.Services.AddDefaultIdentity<User>(options =>
     options.SignIn.RequireConfirmedAccount = false;
     options.Password.RequiredLength = 6;
 })
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<RentACarAppDbContext>();
 
 builder.Services.ConfigureApplicationCookie(options =>
@@ -26,14 +28,6 @@ builder.Services.ConfigureApplicationCookie(options =>
     options.LoginPath = "/User/Login";
 });
 
-
-builder.Services.AddAuthentication()
-    .AddFacebook(options =>
-    {
-        //TODO: Hide them somewhere. They should not be here.
-        options.AppId = "497465629070832";
-        options.AppSecret = "c98187fb17eb7e64ef80cac08cce3b28";
-    });
 
 builder.Services.AddControllersWithViews();
 
@@ -44,6 +38,7 @@ builder.Services.AddTransient<IAgentService, AgentService>();
 
 var app = builder.Build();
 
+app.MapHub<ChatHub>("/chatHub");
 
 if (app.Environment.IsDevelopment())
 {
@@ -63,10 +58,22 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
+
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "default",
+      pattern: "{controller=Home}/{action=Index}/{id?}"
+    );
+
+    endpoints.MapControllerRoute(
+        name: "Areas",
+        pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+    endpoints.MapDefaultControllerRoute();
+    endpoints.MapRazorPages();
+});
 
 
 app.Run();
